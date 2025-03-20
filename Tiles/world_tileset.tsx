b1d11b24f430089627bc75921cf817d8cf663675
Antipandan <jacob@blota.se>
2025-03-20 09:05:01 +0100

    Delete Tiles directory

--- Tiles/world_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="world_tileset" tilewidth="43" tileheight="40" tilecount="30" columns="5">
- <image source="../Spelet/Tiles/sprites/world_tileset.png" width="256" height="256"/>
-</tileset>
