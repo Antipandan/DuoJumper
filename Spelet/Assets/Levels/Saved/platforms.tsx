33336663dab3c842ce94cfbd3d8f09034da6ccec
Antipandan <jacob@blota.se>
2025-03-20 09:06:53 +0100

    Delete Spelet directory

--- Spelet/Assets/Levels/Saved/platforms.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="platforms" tilewidth="32" tileheight="30" tilecount="4" columns="2">
- <image source="sprites/platforms.png" width="64" height="64"/>
-</tileset>
