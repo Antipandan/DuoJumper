e4e7df38317fbdc4a4f4a3a9336ce33670418fd0
Jacob <jacob@blota.se>
2026-07-08 18:37:08 +0200

    whoops oversight

--- Spelet/tile_class.py
+++ Spelet/tile_class.py
@@ -1,3 +1,4 @@
+from pygame.rect import Rect
 from pygame.math import Vector2
 from pygame.surface import Surface
 from DuoJumper.Spelet.player_class import Player
@@ -6,8 +7,8 @@ from constants import pygame
 class Tiles(pygame.sprite.Sprite):
     def __init__(self, pos: Vector2, surface: Surface) -> None:
         super().__init__()
-        self.pos = pos
-        self.image = surface
-        self.rect = self.image.get_rect(topleft=self.pos)
+        self.pos : Vector2 = pos
+        self.image : Surface = surface
+        self.rect: Rect = self.image.get_rect(topleft=self.pos)
         self.owner: Player | None = None
 
